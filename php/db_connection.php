6f0fc2002408c985cee861776fc01eb1962984b7
H.K.D.Sampath Kumara <sampath.kumara@aiesec.net>
2016-11-08 14:36:46 +0530

    Update db_connection.php

--- php/db_connection.php
+++ php/db_connection.php
@@ -1,10 +1,7 @@
 <html>
 <head>
-
 </head>
-
 <body>
-
 <?php
 $servername = "localhost";
 $username = "root";
@@ -16,11 +13,9 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-} 
-
+}
 $sql = "SELECT * FROM appointment";
 $result = $conn->query($sql);
-
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
@@ -33,8 +28,5 @@ if ($result->num_rows > 0) {
 }
 $conn->close();
 ?>
-
-
 </body>
-
 </html>
